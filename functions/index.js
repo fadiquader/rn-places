1038ae256baec69ac3a5ec97db2ece540ff381e6
fadi quader <fadi.quader@gmail.com>
2018-09-18 00:32:16 +0300

    finish uplloading image

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
-const functions = require('firebase-functions');
-const cors = require('cors')({ origin: true });
-const fs = require('fs');
-const UUID = require('uuid/v4');
+const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
+const fs = require("fs");
+const UUID = require("uuid-v4");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -9,36 +9,46 @@ const UUID = require('uuid/v4');
 //  response.send("Hello from Firebase!");
 // });
 
-const gcConfig = {
+// const gsc = require("@google-cloud/storage")({
+//   projectId: 'unwomen-6da62',
+//   keyFilename: 'unwomen.json',
+// });
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage({
   projectId: 'unwomen-6da62',
   keyFilename: 'unwomen.json',
-};
-const gsc = require('@google-cloud/storage')(gcConfig);
+});
 
 exports.storeImage = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
-    const body = JSON.parse(request.body);
+    const body = request.body;
     fs.writeFileSync('/tmp/uploaded-image.jpg', body.image, 'base64', (err) => {
       response.status(500).json({ error: err })
     });
-    const bucket = gsc.bucket('smart-hands-1214f.appspot.com');
+    const bucket = storage.bucket('unwomen-6da62.appspot.com');
     const uuid = UUID();
     bucket.upload('/tmp/uploaded-image.jpg', {
       uploadType: 'media',
       destination: '/places/'+uuid+'.jpg',
       metadata: {
-        contentType: 'image/jpeg',
-        firebaseStorageDownloadTokens: uuid
+        metadata: {
+          contentType: 'image/jpeg',
+          firebaseStorageDownloadTokens: uuid
+        }
       }
     }, (err, file) => {
+      console.log('file ', file)
+      console.log('err ', err)
       if(!err) {
         response.status(201).json({
           imageURL: 'https://firebasestorage.googleapis.com/v0/b/'+
           bucket.name+'/o/'+encodeURIComponent(file.name)+'?alt=media&token='+uuid
         })
+      } else {
+        console.log(err);
+        response.status(500).json({ error: err });
       }
     })
   });
- response.send("Hello from Firebase!");
 });
 
